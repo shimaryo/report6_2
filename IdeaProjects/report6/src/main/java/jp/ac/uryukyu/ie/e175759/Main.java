69219c68fccfc7f8573e08dc5a014bfe4fbde447
shimaryo <e175759@ie.u-ryukyu.ac.jp>
2018-01-16 02:52:51 +0900

    二番目

---
+++ IdeaProjects/report6/src/main/java/jp/ac/uryukyu/ie/e175759/Main.java
@@ -0,0 +1,14 @@
+package jp.ac.uryukyu.ie.e175759;
+
+public class Main {
+    public static void main(String[] args) {
+        Player your = new Player();
+        Player dealer = new Player();
+
+
+        your.Card();
+        dealer.Card();
+        System.out.println("あなたの手札は" + your.card);
+        System.out.println("ディーラーの手札は" + dealer.card);
+    }
+}
---
+++ IdeaProjects/report6/src/main/java/jp/ac/uryukyu/ie/e175759/Player.java
@@ -0,0 +1,20 @@
+package jp.ac.uryukyu.ie.e175759;
+
+public class Player {
+    //public String name;
+    public int card;
+
+    /*public Player(String name){
+       this.name = name;
+    }
+    */
+
+    public void Card(){
+        this.card =(int)(Math.random()*11) + 1;
+
+
+
+    }
+
+}
+
