ee7295d2f62467d7c5a160ddae5775628b149ae8
shimaryo <e175759@ie.u-ryukyu.ac.jp>
2018-01-16 02:55:50 +0900

    最初

--- IdeaProjects/report6/src/main/java/jp/ac/uryukyu/ie/e175759/Main.java
+++ IdeaProjects/report6/src/main/java/jp/ac/uryukyu/ie/e175759/Main.java
@@ -1,5 +1,6 @@
 package jp.ac.uryukyu.ie.e175759;
 
+//
 public class Main {
     public static void main(String[] args) {
         Player your = new Player();
--- IdeaProjects/report6/src/main/java/jp/ac/uryukyu/ie/e175759/Player.java
+++ IdeaProjects/report6/src/main/java/jp/ac/uryukyu/ie/e175759/Player.java
@@ -1,13 +1,10 @@
 package jp.ac.uryukyu.ie.e175759;
 
 public class Player {
-    //public String name;
+
     public int card;
 
-    /*public Player(String name){
-       this.name = name;
-    }
-    */
+
 
     public void Card(){
         this.card =(int)(Math.random()*11) + 1;
