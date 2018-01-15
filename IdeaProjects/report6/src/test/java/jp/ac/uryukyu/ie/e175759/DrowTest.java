ad5d6f6cc509943c108932122be289d7f29d3d31
shimaryo <e175759@ie.u-ryukyu.ac.jp>
2018-01-16 05:27:59 +0900

    ユニットテスト

---
+++ IdeaProjects/report6/src/test/java/jp/ac/uryukyu/ie/e175759/DrowTest.java
@@ -0,0 +1,13 @@
+package jp.ac.uryukyu.ie.e175759;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrowTest {
+    @Test
+    void card() {
+          assertEquals(6,6,5);
+    }
+
+}
